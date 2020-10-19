e6c5b998c2fc4eeddecd5d127ccd643ca42f97e4
YosefGuevara012 <yosef.guevara012@hotmail.com>
2020-10-19 01:34:51 -0500

    ACM to point 3 dog breeds added

--- Libro capitulo 6/ACM_perros.R
+++ Libro capitulo 6/ACM_perros.R
@@ -1,6 +1,14 @@
 library(FactoClass)
+library(MASS)
+library(ade4)
+library(ca)
+library(GDAtools)
+library(writexl)
+
 data(DogBreeds)
 
+
+# 1b
 m <- as.matrix(DogBreeds)
 compare = t(combn(nrow(m),2,FUN=function(x)m[x[1],]==m[x[2],]))
 rownames(compare) = combn(nrow(m),2,FUN=function(x)paste0("Raza",x[1],"_raza",x[2]))
@@ -30,4 +38,14 @@ for(i in 1:length(k)){
     print(k[i])
   }
 }
+
+
+#3 ACM Burt
+
+ACMB <- mjca(DogBreeds[,-7], nd = NA)
+
+burt <- as.data.frame(ACMB$Burt)
+
+write_xlsx(burt,"C:/Users/yosef/OneDrive/Escritorio/Ruptela/burt_2.xlsx")
+
 
